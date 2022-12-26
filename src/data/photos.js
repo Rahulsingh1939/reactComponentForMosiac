1f7fbed0d98f5cd4cd32d4edfdcc0140ccff476d
Rahul singh <rahulsingh19390@gmail.com>
2022-12-26 11:12:53 +0530

    Started Working on Photos Data

---
+++ src/data/photos.js
@@ -0,0 +1,29 @@
+export const photos= [
+    {
+    id: 1,
+    img:"../assets/av1.jpg"
+    },
+    {
+    id: 2,
+    img:"../assets/av2.jpg"
+    },
+    {
+    id: 3,
+    img:"../assets/av3.jpg"
+    },
+    {
+    id: 4,
+    img:"../assets/av4.jpg"
+    },
+    {
+    id: 5,
+    img:"../assets/av5.jpg"
+    },
+    {
+    id: 6,
+    img:"../assets/av6.jpg"
+    },
+    {
+    id: 7,
+    }
+];
